5a91e886715f6f0616a0e052373f41a94056c7f1
EGrayTech <EGrayTech@users.noreply.github.com>
2018-10-29 19:50:53 -0400

    Adding initial files, including the beginning of my home page.

---
+++ src/views/Home.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+import './Home.css';
+
+export default class Home extends Component { 
+  state = { 
+  }
+  
+  render () {                                   
+      return (
+        <div id='container'>
+         <h1 className='title'>APP TITLE</h1>
+        </div>
+      )
+   }
+}
